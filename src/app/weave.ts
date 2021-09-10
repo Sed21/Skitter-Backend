732a42cf74dbb4dec448381f7dc0c7f87ae86bf1
Sed21 <tungstensteel@protonmail.com>
2021-09-10 20:34:33 +0300

    Export weave with correct env initialization

--- src/app/weave.ts
+++ src/app/weave.ts
@@ -1,9 +1,19 @@
 import { App } from '../types';
 import express, { urlencoded } from 'express';
 import morgan from 'morgan';
+import { Routes } from '../routes';
+import { readEnvironment, initDatabase } from '../utils';
 
 export const weave: App = express();
 
+// Set API services
 weave.use(morgan('common'));
 weave.use(urlencoded({ extended: true }));
 weave.use(express.json());
+
+// Set API routes
+weave.use(Routes);
+
+export const configs = readEnvironment();
+
+initDatabase().then(() => (process.env.SKITTER_READY = 'true'));
