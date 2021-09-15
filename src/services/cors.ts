a7bbecee969f7426c242b3034fa839bedf9ab664
Sed21 <tungstensteel@protonmail.com>
2021-09-15 22:59:36 +0300

    Configure CORS policies for API.

---
+++ src/services/cors.ts
@@ -0,0 +1,21 @@
+import * as cors from 'cors';
+
+export const corsOptions: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-AUTH-TOKEN'
+  ],
+  methods: [
+    'GET',
+    'OPTION',
+    'POST',
+    'PUT',
+    'DELETE'
+  ],
+  credentials: true,
+  origin: '*',
+  preflightContinue: false
+};
