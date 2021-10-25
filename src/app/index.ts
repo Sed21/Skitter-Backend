a9004a987636a58f42637c131fe5a51765e4495d
Sed21 <tungstensteel@protonmail.com>
2021-10-25 22:23:32 +0300

    Update application entrypoints.

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,11 +2,12 @@ import * as http from 'http';
 import { weave } from './weave';
 import { Server } from 'http';
 import * as service from '../services';
+import { initUploadLocation } from '../services';
 
 const server: Server = http.createServer(weave);
 
 service
-  .initServices(service.initEnv, service.Database.init)
+  .initServices(service.initEnv, service.Database.init, initUploadLocation)
   .then(() => {
     const config = service.readEnv().server;
     server.listen(config.port, config.host, () => {
--- src/app/weave.ts
+++ src/app/weave.ts
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import { Routes } from '../routes';
 import cors from 'cors';
 import { corsOptions } from '../services';
+import fileUpload from 'express-fileupload';
 
 export const weave: App = express();
 
@@ -12,6 +13,8 @@ weave.use(morgan('common'));
 weave.use(urlencoded({ extended: true }));
 weave.use(express.json());
 weave.use(cors(corsOptions));
+// weave.use(fileUpload(fileUploadOptions));
+weave.use(fileUpload());
 
 // Set API routes
 weave.use(Routes);
